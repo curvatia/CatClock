17de9006f56870cab26a8d341627456cef6bb6db
curvatia <curvatia2002@yahoo.es>
2022-03-10 16:35:58 +0100

    Agregar archivos de proyecto.

---
+++ CatClock/CatClock/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CatClock
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CatClock.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
